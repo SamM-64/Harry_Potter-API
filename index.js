d6a8657d65910dca92e4272e5ced0155943c12d9
Sam-M <moratasamantha@gmail.com>
2023-01-28 04:53:57 +0100

    remove background filter button and add back button

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let personnage = window.localStorage.getItem("personnage");
 
 if (personnage === null) {
 	// Récupération des pièces depuis l'API
-	const reponse = await fetch("https://harry-potter-api-en.onrender.com/characters ");
+	const reponse = await fetch("https://hp-api.onrender.com/api/characters");
 	console.log(reponse);
 	personnage = await reponse.json();
 	// Transformation des pièces en JSON
@@ -35,12 +35,13 @@ function genererPersonnage(personnage) {
 		hogwartsHouseElement.innerText =`House: ${card.hogwartsHouse}`;
 		hogwartsHouseElement.dataset.id = card.hogwartsHouse;
 		const interpretedByElement = document.createElement("p");
-		interpretedByElement.innerText = card.interpretedBy;
+		interpretedByElement.innerText = `Interpreted By: ${card.interpretedBy}`;
 		const childElement = document.createElement("p");
-		childElement.innerText = card.child;
+		childElement.innerText = `Child: ${card.child}`;
+		const patronusElement = document.createElement("p");
+		patronusElement.innerText =`Patronus: ${card.patronus}`;
 		
 		
-
 		// On rattache la balise article a la section Fiches
 		sectionFiches.appendChild(personnageElement);
 		personnageElement.appendChild(imageElement);
@@ -48,16 +49,41 @@ function genererPersonnage(personnage) {
 		personnageElement.appendChild(hogwartsHouseElement);
 		personnageElement.appendChild(interpretedByElement);
 		personnageElement.appendChild(childElement);
-		// pieceElement.appendChild(panierElement);
-		// pieceElement.appendChild(avisBouton);
-		//Code aJouté
-		
-
+		personnageElement.appendChild(patronusElement);
 	}
 
 }
 
 genererPersonnage(personnage);
+
+
+// Bouton filtre
+// Select all of the filter buttons
+const filterButtons = document.querySelectorAll(".filter-button");
+
+// Add a click event listener to each button
+filterButtons.forEach(function(button) {
+	button.addEventListener("click", function() {
+		// Get the value of the data-filter attribute of the button
+		const filterValue = this.dataset.filter;
+
+		// Select all of the elements with the class of "house"
+		const houses = document.querySelectorAll(".house");
+
+		// Loop through each of the elements
+		houses.forEach(function(hogwartsHouse) {
+			// If the element has the class of the filter value, show it
+			if (hogwartsHouse.classList.contains(filterValue)) {
+				hogwartsHouse.style.display = "block";
+			} else {
+				// Otherwise, hide it
+				hogwartsHouse.style.display = "none";
+			}
+		});
+	});
+});
+
+
 
 
 
